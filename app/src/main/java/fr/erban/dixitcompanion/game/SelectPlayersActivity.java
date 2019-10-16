74f7da527b8ed4626c84c510ad4e6b74c6d67392
erbanc <erwan.bancourt@gmail.com>
2019-10-16 16:26:29 +0200

    feat: ajoute l'activité de selection des points pour gagner

--- app/src/main/java/fr/erban/dixitcompanion/game/SelectPlayersActivity.java
+++ app/src/main/java/fr/erban/dixitcompanion/game/SelectPlayersActivity.java
@@ -1,6 +1,8 @@
 package fr.erban.dixitcompanion.game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,11 @@ public class SelectPlayersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_players);
+    }
+
+    public void continueToNumberPoints(View view) {
+
+        Intent intent = new Intent(SelectPlayersActivity.this, SelectNumberPoints.class);
+        SelectPlayersActivity.this.startActivity(intent);
     }
 }
