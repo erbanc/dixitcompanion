39ffdc95ddff1a5f80b0c592eb0e36a51a3b6c1c
erbanc <erwan.bancourt@gmail.com>
2020-10-30 16:01:45 +0100

    feat: modifie l'ajout de joueurs

--- dixit-companion/src/main/java/fr/erban/dxitcompanion/game/activity/SelectPlayersActivity.java
+++ dixit-companion/src/main/java/fr/erban/dxitcompanion/game/activity/SelectPlayersActivity.java
@@ -3,19 +3,26 @@ package fr.erban.dxitcompanion.game.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.textfield.TextInputLayout;
 import fr.erban.dxitcompanion.R;
 import fr.erban.dxitcompanion.game.Creator;
 import fr.erban.dxitcompanion.game.Game;
 import fr.erban.dxitcompanion.game.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class SelectPlayersActivity extends Activity {
 
@@ -29,6 +36,76 @@ public class SelectPlayersActivity extends Activity {
 
         creator = (Creator) getIntent().getSerializableExtra("Creator");
 
+        EditText myTextBox = findViewById(R.id.playerName);
+        final ChipGroup chipGroupPlayers = findViewById(R.id.addedPlayers);
+        final Button addPlayer = findViewById(R.id.addPlayerButton);
+        addPlayer.setEnabled(false);
+        myTextBox.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if(s.toString().trim().isEmpty()) {
+                    addPlayer.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if(s.toString().trim().isEmpty()) {
+                    addPlayer.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                boolean isError = false;
+                TextInputLayout layout = findViewById(R.id.textInputLayout);
+                if (!s.toString().isEmpty() && s.toString().trim().isEmpty()) {
+                    layout.setError(getString(R.string.emptyPlayer));
+                    layout.setErrorEnabled(true);
+                    isError = true;
+                } else {
+                    for (int i=0; i<chipGroupPlayers.getChildCount();i++){
+                        Chip chip = (Chip)chipGroupPlayers.getChildAt(i);
+                        if (chip.getText().toString().equals(s.toString().trim())) {
+                            layout.setError(getString(R.string.playerAlreadyExists));
+                            layout.setErrorEnabled(true);
+                            isError = true;
+                        }
+                    }
+                }
+                if (!isError) {
+                    addPlayer.setEnabled(true);
+                    layout.setError("");
+                    layout.setErrorEnabled(false);
+                } else {
+                    addPlayer.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    public void onAddPlayer(View view) {
+
+        final Chip chip = new Chip(this);
+        final TextView playerName = findViewById(R.id.playerName);
+
+        if (playerName == null || playerName.getText().toString().trim().isEmpty()) {
+            return;
+        }
+        final ChipGroup chipGroupPlayers = findViewById(R.id.addedPlayers);
+
+        chip.setText(playerName.getText().toString());
+        chip.setCloseIconVisible(true);
+        chip.setTextAppearanceResource(R.style.chipText);
+        chip.setCheckable(false);
+        chip.setChecked(true);
+        chip.setOnCloseIconClickListener(v -> chipGroupPlayers.removeView(chip));
+        chip.setHapticFeedbackEnabled(true);
+
+        chipGroupPlayers.addView(chip);
+        playerName.setText("");
     }
 
     public void continueToNumberPoints(View view) {
@@ -36,8 +113,6 @@ public class SelectPlayersActivity extends Activity {
         final List<Player> players = retrievePlayers();
 
         if (verifyNumberOfPlayers(players)) return;
-
-        if (verifyDuplicateNames(players)) return;
 
         final Game game = Game.builder()
                 .players(players)
@@ -63,24 +138,6 @@ public class SelectPlayersActivity extends Activity {
         return false;
     }
 
-    private boolean verifyDuplicateNames(List<Player> players) {
-        List<String> playerNames = new ArrayList<>();
-        for (Player player : players) {
-            for (String name : playerNames) {
-                if (player.getName().equals(name)) {
-                    final Context context = getApplicationContext();
-                    final String message = getString(R.string.duplicatedName);
-                    int duration = Toast.LENGTH_SHORT;
-                    final Toast toast = Toast.makeText(context, message, duration);
-                    toast.show();
-                    return true;
-                }
-            }
-            playerNames.add(player.getName());
-        }
-        return false;
-    }
-
     /**
      * Get the names of the players
      *
@@ -88,63 +145,19 @@ public class SelectPlayersActivity extends Activity {
      */
     private List<Player> retrievePlayers() {
 
-        List<Player> players = new ArrayList<>();
-
-        final EditText player1 = findViewById(R.id.Player1Name);
-
-        addPlayerIfNotEmpty(player1, players);
-
-        final EditText player2 = findViewById(R.id.Player2Name);
-
-        addPlayerIfNotEmpty(player2, players);
-
-        final EditText player3 = findViewById(R.id.Player3Name);
-
-        addPlayerIfNotEmpty(player3, players);
-
-        final EditText player4 = findViewById(R.id.Player4Name);
-
-        addPlayerIfNotEmpty(player4, players);
-
-        final EditText player5 = findViewById(R.id.Player5Name);
-
-        addPlayerIfNotEmpty(player5, players);
-
-        final EditText player6 = findViewById(R.id.Player6Name);
-
-        addPlayerIfNotEmpty(player6, players);
-
-        final EditText player7 = findViewById(R.id.Player7Name);
-
-        addPlayerIfNotEmpty(player7, players);
-
-        final EditText player8 = findViewById(R.id.Player8Name);
-
-        addPlayerIfNotEmpty(player8, players);
-
-        final EditText player9 = findViewById(R.id.Player9Name);
-
-        addPlayerIfNotEmpty(player9, players);
-
-        final EditText player10 = findViewById(R.id.Player10Name);
-
-        addPlayerIfNotEmpty(player10, players);
+        final List<Player> players = new ArrayList<>();
+        final ChipGroup chipGroupPlayers = findViewById(R.id.addedPlayers);
+        for (int i=0; i<chipGroupPlayers.getChildCount();i++){
+            Chip chip = (Chip)chipGroupPlayers.getChildAt(i);
+            if (chip.getText() != null && !chip.getText().toString().trim().isEmpty()) {
+                players.add(Player.builder()
+                        .currentScore(0)
+                        .name(chip.getText().toString())
+                        .scoresheet(new ArrayList<>())
+                        .build());
+            }
+        }
 
         return players;
-    }
-
-    private void addPlayerIfNotEmpty(final EditText player, final List<Player> players) {
-
-        if (player.getText() != null && !player.getText()
-                .toString()
-                .trim()
-                .isEmpty()) {
-            players.add(Player.builder()
-                    .name(player.getText()
-                            .toString())
-                    .currentScore(0)
-                    .scoresheet(new ArrayList<>())
-                    .build());
-        }
     }
 }
